9897004488392897cafc93b585b7059d8a1a59ff
axelahmer <axel.thor.ahmer@gmail.com>
2019-12-16 18:11:18 +1100

    Init commit

---
+++ src/utils/geocode.js
@@ -0,0 +1,23 @@
+const request = require('request');
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoiY29vbHBhbnQiLCJhIjoiY2s0Nm84YWtxMGpmNDNrcW54Ynk4MnRxaSJ9.9AJWGCWYcH6J640oF9s6SA&limit=1';
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services.');
+        } else if (body.features.length === 0) {
+            callback('Unable to find location.');
+        } else {
+            const latitude = body.features[0].center[1];
+            const longitude = body.features[0].center[0];
+            const location = body.features[0].place_name;
+            callback(undefined, {
+                latitude,
+                longitude,
+                location
+            });
+        }
+    });
+};
+
+module.exports = geocode;
